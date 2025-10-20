42f3baa06992004e425faeb230b5a64403752ce3
Ruben100gt <rubengtenza@gmail.com>
2025-10-20 23:18:50 +0200

    Practica 4.02 terminada

--- appVanilla/Practica4.02/biblioteca/Ejercicio2.js
+++ appVanilla/Practica4.02/biblioteca/Ejercicio2.js
@@ -1,17 +1,14 @@
-"use strict";
+'use strict';
 
 const crearTabla = () => {
-	document.body.insertAdjacentHTML("beforeend", '<table id="tabla"></table>');
-	const tabla = document.getElementById("tabla");
+	document.body.insertAdjacentHTML('beforeend', '<table id="tabla"></table>');
+	const tabla = document.getElementById('tabla');
 	for (let i = 0; i < 10; i++) {
-		tabla.insertAdjacentHTML("beforeend", `<tr id="fila${i}"></tr>`);
+		tabla.insertAdjacentHTML('beforeend', `<tr id="fila${i}"></tr>`);
 		const fila = document.getElementById(`fila${i}`);
 		for (let j = 0; j < 10; j++) {
 			let numero = j + i * 10 + 1;
-			fila.insertAdjacentHTML(
-				"beforeend",
-				`<th id="columna${numero}">${numero}</th>`
-			);
+			fila.insertAdjacentHTML('beforeend', `<th id="columna${numero}">${numero}</th>`);
 		}
 	}
 };
@@ -31,7 +28,9 @@ const numeroPrimo = () => {
 		}
 		if (primo) {
 			const celda = document.getElementById(`columna${i}`);
-			celda.classList.add("primo");
+			if (celda) {
+				celda.classList.add('primo');
+			}
 		}
 	}
 };
--- appVanilla/Practica4.02/main.js
+++ appVanilla/Practica4.02/main.js
@@ -1,9 +1,9 @@
-"use strict";
+'use strict';
 
 //imports
-import { remplazarContenido } from "./biblioteca/Ejercicio1.js";
-import { crearTabla, numeroPrimo } from "./biblioteca/Ejercicio2.js";
-import { imagenes } from "./biblioteca/Ejercicio4.js";
+import { remplazarContenido } from './biblioteca/Ejercicio1.js';
+import { crearTabla, numeroPrimo } from './biblioteca/Ejercicio2.js';
+import { imagenes } from './biblioteca/Ejercicio4.js';
 
 setTimeout(() => {
 	remplazarContenido();
@@ -14,4 +14,14 @@ setTimeout(() => {
 	numeroPrimo();
 }, 1000);
 
-imagenes();
+let todasLasImagenes = [
+	'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0LYI5RZWa7OxwS0sibZUeVM6PtxllrRolIg&s',
+	'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQn0gAId9buMttn-AQBe0blrxVDBRa8WsexTUl_77B0dX-w6IQFhGUJUj78mtSD4bKNSQ8&usqp=CAU',
+	'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDMQhIqJwcy0s-LOjpgxPFKA3bnBU0RIjE8zUDuu1L-TLtuKe8QESslUreTl6P6b4xzHQ&usqp=CAU',
+	'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5xGRoX6PofPsSRDo4r16_1xOAHq_0ZsS5CERvsx5Vmw3a4IL_HJEDvksyWonpukt4EHs&usqp=CAU',
+];
+
+imagenes(todasLasImagenes);
+setInterval(() => {
+	imagenes(todasLasImagenes);
+}, 2000);
