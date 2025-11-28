2e4689051185d4e9bf74ff3c87ceae7b34a1afec
Ruben100gt <rubengtenza@gmail.com>
2025-11-28 20:44:54 +0100

    Practica 6.02 falta videos peliculas

--- appVanilla/6.02EnciclopediaStarWars/biblioteca/ejercicio1.js
+++ appVanilla/6.02EnciclopediaStarWars/biblioteca/ejercicio1.js
@@ -1,5 +1,5 @@
-'use strict';
-import { formatearFecha } from '../../biblioteca.js';
+"use strict";
+import { formatearFecha } from "../../biblioteca.js";
 
 //Traer datos
 const traerPeliculas = (url) => {
@@ -18,27 +18,29 @@ const traerPeliculas = (url) => {
 //Dibujar datos
 
 const dibujarPeliculas = (peliculas) => {
-	let plantilla = '';
+	let plantilla = `<h4 class="mensaje">Al pulsar cualquier película exime la responsibilidad de los vídeos al creador de la práctica.</h4>`;
 	Array.isArray(peliculas) && peliculas.length
 		? peliculas.map((peli) => {
-				plantilla += `<li class="pelicula"><p class="id">${peli.episode_id}</p><h2 id=${peli.episode_id} class="titulo">${peli.title}</h2></li>`;
+				plantilla += `<li class="pelicula"><h3 id=${peli.episode_id} class="titulo">${peli.title}</h3><h4 class="id">Peli: ${peli.episode_id}</h4></li>`;
 		  })
-		: (plantilla = '<h3>No se han encontrado películas.</h3>');
+		: (plantilla = "<h3>No se han encontrado películas.</h3>");
 
 	return plantilla;
 };
 
 const dibujarPeliculaDetalle = (peliculas, id) => {
-	let plantilla = '';
+	let plantilla = "";
 	const idPeli = parseInt(id);
 	const pelicula = peliculas.filter((p) => p.episode_id === idPeli)[0];
 	if (pelicula.length === 0) {
-		return '<h3>No se ha encontrado la película con ese id.</h3>';
+		return "<h3>No se ha encontrado la película con ese id.</h3>";
 	}
 
-	const fecha = pelicula.release_date ? formatearFecha(pelicula.release_date) : 'Fecha desconocida.';
+	const fecha = pelicula.release_date
+		? formatearFecha(pelicula.release_date)
+		: "Fecha desconocida.";
 
-	plantilla += `<div class="tituloPeli"><h2>${pelicula.title}</h2><p class="datosPeli">Director: ${pelicula.director}<br/>Productor: ${pelicula.producer}<br/>Fecha de lanzamiento: ${fecha}</p></div>`;
+	plantilla += `<div class="infoPeli"><h2>${pelicula.title}</h2><cite class="datosPeli"><strong>- Director:</strong> ${pelicula.director}<br/><strong>- Productor:</strong> ${pelicula.producer}<br/><strong>- Fecha de lanzamiento:</strong> ${fecha}<br/><strong>- Sinopsis:</strong> ${pelicula.opening_crawl}</cite></div>`;
 	return plantilla;
 };
 
--- appVanilla/6.02EnciclopediaStarWars/main.js
+++ appVanilla/6.02EnciclopediaStarWars/main.js
@@ -1,13 +1,20 @@
-'use strict';
+"use strict";
 
 //imports
-import { traerPeliculas, dibujarPeliculas, dibujarPeliculaDetalle } from './biblioteca/ejercicio1.js';
+import {
+	traerPeliculas,
+	dibujarPeliculas,
+	dibujarPeliculaDetalle,
+} from "./biblioteca/ejercicio1.js";
 
 window.onload = () => {
-	const peliculas = document.getElementById('peliculas');
-	const informacion = document.getElementById('informacion');
-	const url = 'https://swapi.dev/api/films/';
+	const peliculas = document.getElementById("peliculas");
+	const informacion = document.getElementById("informacion");
+	const contInf = document.getElementById("contenedorInformacion");
+	const url = "https://swapi.dev/api/films/";
 	let pelis = [];
+	const video = document.getElementById("fondoVideo");
+	video.src = `./assets/espacio.mp4`;
 
 	const pintarPeliculas = async () => {
 		pelis = await traerPeliculas(url);
@@ -17,11 +24,22 @@ window.onload = () => {
 	//Pintamos los planetas nada más cargar la página
 	pintarPeliculas();
 
-	peliculas.addEventListener('click', (evento) => {
-		if (evento.target.classList.contains('titulo')) {
+	peliculas.addEventListener("click", (evento) => {
+		if (evento.target.classList.contains("titulo")) {
 			const id = evento.target.id;
-			informacion.innerHTML = dibujarPeliculaDetalle(pelis, id);
+
+			video.src = `./assets/transicion.mp4`;
+			informacion.classList.add("ocultarTexto");
+			informacion.offsetHeight;
+
+			setTimeout(() => {
+				video.src = `./assets/peli${id}.mp4`;
+				informacion.classList.remove("ocultarTexto");
+				contInf.classList.remove("ocultar");
+				informacion.innerHTML = dibujarPeliculaDetalle(pelis, id);
+			}, 4000);
 		}
 	}),
 		false;
 };
+//video peli 2: https://www.youtube.com/watch?v=s8IyWtX9ImQ
