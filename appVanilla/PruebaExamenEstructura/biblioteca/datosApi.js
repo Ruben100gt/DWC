9d9ba5083ac8457a5da7ed53ee50ae4664df4cee
Ruben100gt <rubengtenza@gmail.com>
2025-12-16 17:40:12 +0100

    PruebaExamenEstructura continueado insti

--- appVanilla/PruebaExamenEstructura/biblioteca/datosApi.js
+++ appVanilla/PruebaExamenEstructura/biblioteca/datosApi.js
@@ -1,30 +1,29 @@
-'use strict';
+"use strict";
 
 let planetasPropios = [];
-localStorage.getItem('planetas')
-	? JSON.parse(localStorage.getItem('planetas'))
-	: JSON.stringify(localStorage.setItem('planetas', planetasPropios));
+localStorage.getItem("planetas")
+	? JSON.parse(localStorage.getItem("planetas"))
+	: JSON.stringify(localStorage.setItem("planetas", planetasPropios));
 
 const traerDatos = async (url) => {
 	try {
 		let respuesta = await fetch(url);
-
 		if (respuesta.ok) {
 			respuesta = await respuesta.json();
 			return respuesta.results;
 		} else {
-			throw new Error('Se ha producido un error al traer los datos.');
+			throw new Error("Se ha producido un error al traer los datos.");
 		}
 	} catch (error) {
 		throw error;
 	}
 };
 
-const traerMuchosDatos = async (ulrs) => {
-	let promesas = await ulrs.map((url) => {
-		traerDatos(url);
+const traerMuchosDatos = async (urls) => {
+	let promesas = urls.map((url) => {
+		return traerDatos(url);
 	});
-	let promesasHechas = await Promise.allSettled(promesas);
+	let promesasHechas = await Promise.any(promesas);
 	return promesasHechas;
 };
 
--- appVanilla/PruebaExamenEstructura/main.js
+++ appVanilla/PruebaExamenEstructura/main.js
@@ -1,45 +1,50 @@
-'use strict';
+"use strict";
 
 //imports
-import { datosFormulario } from './biblioteca/biblioteca.js';
-import { traerDatos } from './biblioteca/datosApi.js';
-import { validarDatos } from './biblioteca/validar.js';
+import { datosFormulario } from "./biblioteca/biblioteca.js";
+import { traerDatos, traerMuchosDatos } from "./biblioteca/datosApi.js";
+import { validarDatos } from "./biblioteca/validar.js";
 
 window.onload = () => {
 	let planetas = [];
 	let datosPlaneta = {};
 	let datos = [];
 	const formulario = document.forms[0];
-	let url = 'http://swapi.py4e.com/api/people';
+	let url = "http://swapi.py4e.com/api/people";
+	let urls = [
+		"http://swapi.py4e.com/api/people",
+		"http://swapi.py4e.com/api/people",
+		"http://swapi.py4e.com/api/people",
+	];
 
-	if (typeof Storage !== 'undefined') {
-		const obtenerPlanetas = async (url) => {
+	if (typeof Storage !== "undefined") {
+		const obtenerPlanetas = async () => {
 			try {
 				datos = await traerDatos(url);
-
+				console.log(datos);
 				formulario.addEventListener(
-					'click',
+					"click",
 					(evento) => {
-						if (evento.target.type === 'button') {
+						if (evento.target.type === "button") {
 							if (evento.target.nextElementSibling) {
 								datosPlaneta = datosFormulario(formulario);
-								validarDatos(datosPlaneta) && guardarPlanetas();
-								planetas = [...planetas, datos];
-								//guardar localstorage
+								if (validarDatos(datosPlaneta)) {
+									guardarPlanetas();
+									planetas = [...planetas, datos];
+									localStorage.setItem("planetas", JSON.parse(planetas));
+								}
 							} else {
 							}
 						}
 					},
 					false
 				);
-
-				console.log('hola');
 			} catch (error) {
 				console.log(error.message);
 			}
 		};
-		obtenerPlanetas(url);
+		obtenerPlanetas();
 	} else {
-		console.error('Este navegador no soporta la API localStorage.');
+		console.error("Este navegador no soporta la API localStorage.");
 	}
 };
