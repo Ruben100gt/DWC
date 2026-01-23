b1b14cde9bbfe59a4e21414dc20193c16df81f0b
Ruben100gt <rubengtenza@gmail.com>
2026-01-23 15:55:23 +0100

    listaCompra - proveedorSesion hecho, falta separar useSesion

--- React/listaCompra/src/context/ProveedorSesion.jsx
+++ React/listaCompra/src/context/ProveedorSesion.jsx
@@ -16,7 +16,7 @@ const ProveedorSesion = ({ children }) => {
 	const [datosSesion, sertDatosSesion] = useState(datosSesionInicial);
 	const [usuario, setUsuario] = useState(usuarioInicial);
 	const [errorUsuario, setErrorUsuario] = useState(errorUsuarioInicial);
-	const { seionIniciada, setSesionIniciada } = useState(sesionIniciadaInicial);
+	const { sesionIniciada, setSesionIniciada } = useState(sesionIniciadaInicial);
 
 	const crearCuenta = async () => {
 		try {
@@ -36,4 +36,85 @@ const ProveedorSesion = ({ children }) => {
 			setErrorUsuario(error.message);
 		}
 	};
+
+	const iniciarSesionPassword = async () => {
+		setErrorUsuario(errorUsuarioInicial);
+		try {
+			const { data, error } = await supabaseConexion.auth.signInWithPassword({
+				email: datosSesion.email,
+				password: datosSesion.password,
+				options: {
+					emailRedirectTo: "http://localhost:5173/",
+				},
+			});
+			if (error) {
+				throw error;
+			}
+		} catch (error) {
+			setErrorUsuario(error.message);
+		}
+	};
+
+	const cerrarSesion = async () => {
+		try {
+			await supabaseConexion.auth.signOut();
+			setErrorUsuario(errorUsuarioInicial);
+			navegar("/supabase/inicio");
+		} catch (error) {
+			setErrorUsuario(error.message);
+		}
+	};
+
+	const obtenerUsuario = async () => {
+		try {
+			const { data, error } = await supabaseConexion.auth.getUser();
+
+			if (error) {
+				throw error;
+			}
+			setUsuario(data.user);
+			setErrorUsuario(errorUsuarioInicial);
+		} catch (error) {
+			setErrorUsuario(error.message);
+		}
+	};
+
+	const actualizarDato = (evento) => {
+		const { name, value } = evento.target;
+		sertDatosSesion({ ...datosSesion, [name]: value });
+	};
+
+	useEffect(() => {
+		const suscripcion = supabaseConexion.auth.onAuthStateChange(
+			(event, session) => {
+				if (session) {
+					navegar("/supabase/listado");
+					setSesionIniciada(true);
+					obtenerUsuario();
+				} else {
+					navegar("supabase/IniciarSesion");
+					setSesionIniciada(false);
+				}
+			},
+		);
+	}, []);
+
+	const datosProveer = {
+		crearCuenta,
+		iniciarSesionPassword,
+		cerrarSesion,
+		actualizarDato,
+		sesionIniciada,
+		usuario,
+		errorUsuario,
+	};
+
+	return (
+		<contextoSesion.Provider value={datosProveer}>
+			{children}
+		</contextoSesion.Provider>
+	);
 };
+
+export default ProveedorSesion;
+export { contextoSesion };
